6171dc7021812a91c45bf5d9eafc3e70ba8c976e
Wei Hu <96912272+PreX-AW@users.noreply.github.com>
2023-02-16 17:19:32 +0800

    update_SLT_test

--- Date_structure_test/test.c
+++ Date_structure_test/test.c
@@ -4,16 +4,39 @@
 
 
 #include"SLT.h"
+
 void test1() {
 	SLTNode* plist = SLTCreate(10);
+
 	SLTprint(plist);
 
 
 }
+void test2() {
+	SLTNode* plist = SLTCreate(1);
+	SLTPushBack(&plist);
+	SLTprint(plist);
+	SLTPopBack(&plist);
+	SLTprint(plist);
+}
+void test3() {
+	SLTNode* plist = SLTCreate(0);
+	SLTPushFront(&plist);
+	SLTprint(plist);
+	SLTPopFront(&plist);
+	SLTprint(plist);
+}
 
 int main() {
 
-	test1();
+	//test1();
+	//test2();
+	//test3();
+
+
+
+
+
 
 	return 0;
 }
