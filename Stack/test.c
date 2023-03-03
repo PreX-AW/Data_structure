2cfc325775b03189a6c18f84a075cbd09259a257
Wei Hu <huwei040215@gmail.com>
2023-03-03 14:07:46 +0800

    complement

--- Stack/test.c
+++ Stack/test.c
@@ -6,11 +6,38 @@ void test1()
 {
 StkInit(&sk);
 }
+void test2(){
+   StkPush(&sk,1);
+   StkPush(&sk,2);
+   StkPush(&sk,3);
+   StkPush(&sk,4);
+   StkPush(&sk,5);
+   StkPush(&sk,6);
+   StkPush(&sk,7);
+}
+
+void test3(){
+   StkPop(&sk);
+   StkPop(&sk);
+   StkPop(&sk);
+   StkPop(&sk);
+   StkPop(&sk);
+   StkPop(&sk);
+   StkPop(&sk);
+   StkPop(&sk);
+   StkPop(&sk);
+}
 
 
+void test4(){
 
+    printf("%d\n",StkTop(&sk));
+
+    StkEmpty(&sk);
+}
 int main(){
     test1();
+    StkDestory(&sk);
 
 
 
